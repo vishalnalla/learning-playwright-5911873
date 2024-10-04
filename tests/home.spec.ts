c828eb2de3ed5c192ac14b27e426a6ebcf33c635
Butch Mayhew <bmayhew@tilled.com>
2024-10-04 16:27:18 -0500

    adding structure to home.spec.ts file

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,21 +1,30 @@
 import { test, expect } from "@playwright/test";
 
-test("Home page", async ({ page }) => {
-  await page.goto("https://practicesoftwaretesting.com/");
-  // Esnure the sign-in link is present
-  await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign in");
+test.describe("Home page", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://practicesoftwaretesting.com/");
+  });
+  test("check sign in", async ({ page }) => {
+    await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign in");
+  });
 
-  // Check the title of the page
-  await expect(page).toHaveTitle("Practice Software Testing - Toolshop - v5.0");
+  test("validate page title", async ({ page }) => {
+    await expect(page).toHaveTitle(
+      "Practice Software Testing - Toolshop - v5.0"
+    );
+  });
 
-  // Check the count of items displayed
-  const productGrid = page.locator(".col-md-9");
-  await expect(productGrid.getByRole("link")).toHaveCount(9);
-  expect(await productGrid.getByRole("link").count()).toBe(9);
+  test("grid loads with 9 items", async ({ page }) => {
+    const productGrid = page.locator(".col-md-9");
+    await expect(productGrid.getByRole("link")).toHaveCount(9);
+    expect(await productGrid.getByRole("link").count()).toBe(9);
+  });
 
-  // Search for Thor Hammer and check the result
-  await page.getByTestId("search-query").fill("Thor Hammer");
-  await page.getByTestId("search-submit").click();
-  await expect(productGrid.getByRole("link")).toHaveCount(1);
-  await expect(page.getByAltText("Thor Hammer")).toBeVisible();
+  test("search for Thor Hammer", async ({ page }) => {
+    const productGrid = page.locator(".col-md-9");
+    await page.getByTestId("search-query").fill("Thor Hammer");
+    await page.getByTestId("search-submit").click();
+    await expect(productGrid.getByRole("link")).toHaveCount(1);
+    await expect(page.getByAltText("Thor Hammer")).toBeVisible();
+  });
 });
