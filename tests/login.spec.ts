8c624ad3c1c7cd6126cf59d4c60728a6dd7329fb
Butch Mayhew <bmayhew@tilled.com>
2024-10-05 01:18:40 -0500

    adding a page object model example

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
+import { LoginPage } from "../pages/login/loginPage";
 
-test("login test", async ({ page }) => {
+test("login without page object", async ({ page }) => {
   await page.goto("https://practicesoftwaretesting.com/");
   await page.locator('[data-test="nav-sign-in"]').click();
   await page
@@ -14,4 +15,13 @@ test("login test", async ({ page }) => {
   await expect(page.locator('[data-test="page-title"]')).toContainText(
     "My account"
   );
+});
+
+test("Login with page object", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  await loginPage.emailInput.fill("customer@practicesoftwaretesting.com");
+  await loginPage.passwordInput.fill("welcome01");
+  await loginPage.loginButton.click();
+  await expect(page.getByTestId("nav-menu")).toContainText("Jane Doe");
 });
